85ed67f43136a03c38df587e2c8cddb55df68203
Arun Isaac <arunisaac@systemreboot.net>
2021-12-13 18:30:27 +0530

    Upgrade to bleeding edge (ccwl graphviz).
    This fixes a few bugs and brings in new features from (ccwl graphviz).
    
    * guix.scm: Import (gnu packages autotools), (guix git-download)
    and (guix packages). Prefix (gnu packages bioinformatics) imports with
    guix:.
    (ccwl): New variable.

--- guix.scm
+++ guix.scm
@@ -4,10 +4,34 @@
 ;;
 ;; Happy hacking!
 
-(use-modules (gnu packages bioinformatics)
+(use-modules (gnu packages autotools)
+             ((gnu packages bioinformatics) #:prefix guix:)
              (gnu packages graphviz)
              (gnu packages guile)
-             (gnu packages guile-xyz))
+             (gnu packages guile-xyz)
+             (guix git-download)
+             (guix packages))
+
+(define ccwl
+  (let ((commit "1e36c8bdc8c22dee68a3aa292c1d318bd8e0b982"))
+    (package
+      (inherit guix:ccwl)
+      (name "ccwl")
+      (version (git-version (package-version guix:ccwl) "0" commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/arunisaac/ccwl")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32
+           "1fcgxjxf5c7329pdd16fgz6crvg4jz84czp7kkydj99cgg2f5rkx"))))
+      (native-inputs
+       `(("autoconf" ,autoconf)
+         ("automake" ,automake)
+         ,@(package-native-inputs guix:ccwl))))))
 
 (packages->manifest
  (list guile-3.0 guile-dbi guile-dbd-mysql
