40d9326cf7a94340b32aad0c1d688a45bcd36427
Arun Isaac <arunisaac@systemreboot.net>
2022-03-10 13:27:34 +0530

    Use run64 to run tests.
    * manifest.scm: Import (guix build-system gnu) and (guix licenses).
    (run64): New variable.
    Add run64 to manifest.
    * Makefile (GUILE): Delete variable.
    (RUN64): New variable.
    (check): Use run64 instead of guile to run tests.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-GUILE = guile
+RUN64 = guile-run64
 
 .PHONY: check
 check: tests.scm
-	GUILE_LOAD_PATH=$(GUILE_LOAD_PATH):. $(GUILE) $^
+	GUILE_LOAD_PATH=$(GUILE_LOAD_PATH):. $(RUN64) $^
--- manifest.scm
+++ manifest.scm
@@ -9,7 +9,9 @@
              (gnu packages graphviz)
              (gnu packages guile)
              (gnu packages guile-xyz)
+             (guix build-system gnu)
              (guix git-download)
+             ((guix licenses) #:prefix license:)
              (guix packages))
 
 (define ccwl
@@ -33,8 +35,32 @@
          ("automake" ,automake)
          ,@(package-native-inputs guix:ccwl))))))
 
+(define run64
+  (package
+    (name "run64")
+    (version "0.1.0")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://git.systemreboot.net/run64")
+                    (commit "e07c1f90f5436559839dea7c0231dd3ee36678ce")))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "1la0x80b9s6am3hag0ijnvli3fzaa4iiiqm7dmwnyyhpd6n24jqn"))))
+    (build-system gnu-build-system)
+    (arguments
+     `(#:make-flags (list (string-append "prefix=" %output))
+       #:phases
+       (modify-phases %standard-phases
+         (delete 'configure))))
+    (home-page "https://run64.systemreboot.net")
+    (synopsis "SRFI-64 test runner for Scheme")
+    (description "run64 is a SRFI-64 test runner for Scheme.")
+    (license license:gpl3+)))
+
 (packages->manifest
  (list guile-3.0 guile-dbi guile-dbd-mysql
        ;; We abuse (ccwl graphviz) as a library to visualize the database
        ;; schema. Hence we need ccwl and guile-libyaml.
-       ccwl graphviz guile-libyaml guile-sparql))
+       ccwl graphviz guix:guile-libyaml guile-sparql run64))
