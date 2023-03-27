8580d4048a73b3214ac5ee70f39781b6490aa27f
Munyoki Kilyungi <me@bonfacemunyoki.com>
2023-03-27 22:50:36 +0300

    Split substring int a list of substrings delimited by substring
    * dump.scm (string-split-substring): New function.
    
    Signed-off-by: Munyoki Kilyungi <me@bonfacemunyoki.com>

--- dump.scm
+++ dump.scm
@@ -98,6 +98,27 @@
     (string->symbol
      (format #f "~s~a" field schema))))
 
+(define (string-split-substring str substr)
+  "Split the string @var{str} into a list of substrings delimited by the
+substring @var{substr}."
+
+  (define substrlen (string-length substr))
+  (define strlen (string-length str))
+
+  (define (loop index start)
+    (cond
+     ((>= start strlen) (list ""))
+     ((not index) (list (substring str start)))
+     (else
+      (cons (substring str start index)
+            (let ((new-start (+ index substrlen)))
+              (loop (string-contains str substr new-start)
+                    new-start))))))
+
+  (cond
+   ((string-contains str substr) => (lambda (idx) (loop idx 0)))
+   (else (list str))))
+
 (define (delete-substrings str . substrings)
   "Delete SUBSTRINGS, a list of strings, from STR."
   (fold (lambda (substring result)
