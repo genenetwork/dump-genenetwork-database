53c519c6d07992a0249e8f365f8b1a1af7a13886
Munyoki Kilyungi <me@bonfacemunyoki.com>
2023-05-26 08:35:54 +0300

    Create a new probesetfreeze dump
    This way, the probeset dump will become smaller.
    
    Signed-off-by: Munyoki Kilyungi <me@bonfacemunyoki.com>

--- examples/dump-probeset.scm
+++ examples/dump-probeset.scm
@@ -20,14 +20,6 @@
   (list-ref (command-line) 2))
 
 
-(define-dump dump-gene-chip
-  (tables (GeneChip))
-  (schema-triples
-   (gn:name rdfs:range rdfs:Literal))
-  (triples (string->identifier "platform" (field GeneChip Name))
-    (set rdf:type 'gn:platform)
-    (set gn:name (field GeneChip GeneChipName))))
-
 (define-dump dump-probeset
   (tables (ProbeSet
            (left-join ProbeSetXRef "ON ProbeSetXRef.ProbeSetId = ProbeSet.Id")
@@ -144,35 +136,6 @@
     (set gn:secondaryNames (field ProbeSet SecondaryNames))
     (set gn:peptideSequence (field ProbeSet PeptideSequence))))
 
-;; Molecular Traits are also referred to as ProbeSets
-(define-dump dump-probesetfreeze
-  (tables (ProbeSetFreeze
-           (left-join InfoFiles "ON InfoFiles.InfoPageName = ProbeSetFreeze.Name")
-           (left-join ProbeFreeze "USING (ProbeFreezeId)")
-           (left-join AvgMethod "ON AvgMethod.AvgMethodId = ProbeSetFreeze.AvgID")
-           (left-join InbredSet "ON ProbeFreeze.InbredSetId=InbredSet.Id")
-           (left-join Tissue "USING (TissueId)"))
-          "WHERE ProbeSetFreeze.public > 0 AND InfoFiles.InfoPageName IS NULL GROUP BY ProbeFreeze.Id")
-  (schema-triples
-   (gn:avgMethod rdfs:range rdfs:Literal)
-   (gn:dataScale rdfs:range rdfs:Literal))
-  (triples
-      (ontology 'probeset:
-                (regexp-substitute/global #f "[^A-Za-z0-9:]"
-                                          (field ProbeSetFreeze Name)
-                                          'pre "_" 'post))
-    (set rdf:type 'gn:probesetDataset)
-    (set gn:avgMethod (string->identifier "avgmethod" (field AvgMethod Name)))
-    (set gn:fullName (field ProbeSetFreeze FullName))
-    (set gn:shortName (field ProbeSetFreeze ShortName))
-    (set dct:created (annotate-field
-                      (field ProbeSetFreeze CreateTime)
-                      '^^xsd:datetime))
-    (set gn:dataScale (field ProbeSetFreeze DataScale))
-    (set gn:tissueName (string->identifier "tissue" (field Tissue Short_Name)))
-    (set gn:datasetOfInbredSet
-         (string->identifier "inbredSet" (field InbredSet Name InbredSetName)))))
-
 
 
 (call-with-target-database
@@ -203,7 +166,5 @@
        (prefix "xsd:" "<http://www.w3.org/2001/XMLSchema#>")
        (prefix "probeset:" "<http://genenetwork.org/probeset/>")
        (newline)
-       (dump-gene-chip db)
-       (dump-probesetfreeze db)
        (dump-probeset db))
      #:encoding "utf8")))
