b33f7f7218d0786a6c5382e56afa639dfcd15194
Munyoki Kilyungi <me@bonfacemunyoki.com>
2023-07-26 13:43:33 +0300

    Replace "gn-term" with "gnt" prefix
    Signed-off-by: Munyoki Kilyungi <me@bonfacemunyoki.com>

--- examples/dump-phenotype.scm
+++ examples/dump-phenotype.scm
@@ -27,10 +27,10 @@
            (left-join InbredSet "ON PublishFreeze.InbredSetId = InbredSet.InbredSetId"))
           "WHERE PublishFreeze.public > 0 AND PublishFreeze.confidentiality < 1 AND InfoFiles.InfoPageName IS NULL")
   (schema-triples
-   (gn-term:datasetOfInbredSet rdfs:range gn:inbredSet)
-   (gn-term:name rdfs:range rdfs:Literal)
-   (gn-term:fullName rdfs:range rdfs:Literal)
-   (gn-term:shortName rdfs:range rdfs:Literal)
+   (gnt:datasetOfInbredSet rdfs:range gn:inbredSet)
+   (gnt:name rdfs:range rdfs:Literal)
+   (gnt:fullName rdfs:range rdfs:Literal)
+   (gnt:shortName rdfs:range rdfs:Literal)
    (gn:phenotypeDataset rdf:subClassOf gn:dataset))
   (triples
       (string->identifier
@@ -41,13 +41,13 @@
        #:separator ""
        #:proc string-capitalize-first)
     (set rdf:type 'gn:phenotypeDataset)
-    (set gn-term:name (field PublishFreeze Name))
-    (set gn-term:fullName (field PublishFreeze FullName))
-    (set gn-term:shortName (field PublishFreeze ShortName))
+    (set gnt:name (field PublishFreeze Name))
+    (set gnt:fullName (field PublishFreeze FullName))
+    (set gnt:shortName (field PublishFreeze ShortName))
     (set dct:created (annotate-field
                       (field PublishFreeze CreateTime)
                       '^^xsd:date))
-    (set gn-term:datasetOfInbredSet
+    (set gnt:datasetOfInbredSet
          (string->identifier "inbredSet" (field InbredSet Name InbredSetName)))))
 
 (define-dump dump-phenotypes
@@ -58,19 +58,19 @@
            (left-join InfoFiles "ON InfoFiles.InfoPageName = PublishFreeze.Name")))
   (schema-triples
    (gn:phenotypeDataset rdfs:subPropertyOf gn:dataset)
-   (gn-term:publicationDescription rdfs:range rdfs:Literal)
-   (gn-term:originalDescription rdfs:range rdfs:Literal)
-   (gn-term:prePublicationDescription rdfs:range rdfs:Literal)
-   (gn-term:postPublicationAbbreviation rdfs:range rdfs:Literal)
-   (gn-term:labCode rdfs:range rdfs:Literal)
-   (gn-term:submitter rdfs:range rdfs:Literal)
-   (gn-term:owner rdfs:range rdfs:Literal)
-   (gn-term:mean rdfs:range xsd:double)
-   (gn-term:LRS rdfs:range xsd:float)
-   (gn-term:locus rdfs:range rdfs:Literal)
-   (gn-term:additive rdfs:range xsd:decimal)
-   (gn-term:sequence rdfs:range rdfs:Literal)
-   (gn-term:phenotypeOfPublication rdfs:range gn-term:pubMedId))
+   (gnt:publicationDescription rdfs:range rdfs:Literal)
+   (gnt:originalDescription rdfs:range rdfs:Literal)
+   (gnt:prePublicationDescription rdfs:range rdfs:Literal)
+   (gnt:postPublicationAbbreviation rdfs:range rdfs:Literal)
+   (gnt:labCode rdfs:range rdfs:Literal)
+   (gnt:submitter rdfs:range rdfs:Literal)
+   (gnt:owner rdfs:range rdfs:Literal)
+   (gnt:mean rdfs:range xsd:double)
+   (gnt:LRS rdfs:range xsd:float)
+   (gnt:locus rdfs:range rdfs:Literal)
+   (gnt:additive rdfs:range xsd:decimal)
+   (gnt:sequence rdfs:range rdfs:Literal)
+   (gnt:phenotypeOfPublication rdfs:range gn-term:pubMedId))
   (triples (string->identifier
             ""
             (regexp-substitute/global #f "[^A-Za-z0-9:]"
@@ -79,44 +79,44 @@
             #:separator ""
             #:proc string-capitalize-first)
     (set rdf:type 'gn:phenotype)
-    (set gn-term:name (sanitize-rdf-string
+    (set gnt:name (sanitize-rdf-string
                   (field
                    ("CAST(CONVERT(BINARY CONVERT(IF(Phenotype.Post_publication_abbreviation IS NULL, IF(Phenotype.Pre_publication_abbreviation IS NULL, Phenotype.Id, Phenotype.Pre_publication_abbreviation), Phenotype.Post_publication_abbreviation) USING latin1) USING utf8) AS VARCHAR(100))"
                     PhenotypeName))))
     ;; There is no row with an empty post-publication description so
     ;; use this field as the main publication description
-    (set gn-term:publicationDescription
+    (set gnt:publicationDescription
          (sanitize-rdf-string
           (field ("CAST(CONVERT(BINARY CONVERT(Phenotype.Post_publication_description USING latin1) USING utf8) AS CHAR(10000))"
                   postPubDescr))))
-    (set gn-term:originalDescription (sanitize-rdf-string
+    (set gnt:originalDescription (sanitize-rdf-string
                                  (delete-substrings
                                   (field Phenotype Original_description)
                                   "Original post publication description: ")))
-    (set gn-term:prePublicationDescription
+    (set gnt:prePublicationDescription
          (sanitize-rdf-string
           (field
            ("CAST(CONVERT(BINARY CONVERT(Phenotype.Pre_publication_description USING latin1) USING utf8) AS VARCHAR(15000))"
             prePubDesc))))
-    (set gn-term:prePublicationAbbreviation (sanitize-rdf-string (field Phenotype Pre_publication_abbreviation)))
-    (set gn-term:postPublicationAbbreviation (sanitize-rdf-string (field Phenotype Post_publication_abbreviation)))
-    (set gn-term:labCode (field Phenotype Lab_code))
-    (set gn-term:submitter (sanitize-rdf-string (field Phenotype Submitter)))
-    (set gn-term:owner (sanitize-rdf-string (field Phenotype Owner)))
-    (set gn-term:mean (annotate-field (field ("IFNULL(PublishXRef.mean, '')" mean))
+    (set gnt:prePublicationAbbreviation (sanitize-rdf-string (field Phenotype Pre_publication_abbreviation)))
+    (set gnt:postPublicationAbbreviation (sanitize-rdf-string (field Phenotype Post_publication_abbreviation)))
+    (set gnt:labCode (field Phenotype Lab_code))
+    (set gnt:submitter (sanitize-rdf-string (field Phenotype Submitter)))
+    (set gnt:owner (sanitize-rdf-string (field Phenotype Owner)))
+    (set gnt:mean (annotate-field (field ("IFNULL(PublishXRef.mean, '')" mean))
                                  '^^xsd:double))
-    (set gn-term:locus (field PublishXRef Locus))
-    (set gn-term:LRS (annotate-field (field ("IFNULL(PublishXRef.LRS, '')" lrs)) '^^xsd:float))
-    (set gn-term:additive (annotate-field (field ("IFNULL(PublishXRef.additive, '')" additive)) '^^xsd:decimal))
-    (set gn-term:sequence (annotate-field (field PublishXRef Sequence) '^^xsd:int))
-    (set gn-term:phenotypeOfDataset
+    (set gnt:locus (field PublishXRef Locus))
+    (set gnt:LRS (annotate-field (field ("IFNULL(PublishXRef.LRS, '')" lrs)) '^^xsd:float))
+    (set gnt:additive (annotate-field (field ("IFNULL(PublishXRef.additive, '')" additive)) '^^xsd:decimal))
+    (set gnt:sequence (annotate-field (field PublishXRef Sequence) '^^xsd:int))
+    (set gnt:phenotypeOfDataset
          (string->identifier
           ""
           (field
            ("IFNULL(InfoFiles.InfoPageName, IFNULL(PublishFreeze.Name, ''))" DatasetName))
           #:separator ""
           #:proc string-capitalize-first))
-    (set gn-term:phenotypeOfPublication
+    (set gnt:phenotypeOfPublication
          (let ((pmid (field
                       ("IF(Publication.PubMed_ID IS NULL, '', CONVERT(Publication.PubMed_Id, INT))"
                        pmid)))
@@ -134,7 +134,7 @@
  (prefixes
   '(("dct:" "<http://purl.org/dc/terms/>")
     ("gn:" "<http://genenetwork.org/id/>")
-    ("gn-term:" "<http://genenetwork.org/terms/>")
+    ("gnt:" "<http://genenetwork.org/terms/>")
     ("rdf:" "<http://www.w3.org/1999/02/22-rdf-syntax-ns#>")
     ("rdfs:" "<http://www.w3.org/2000/01/rdf-schema#>")
     ("xsd:" "<http://www.w3.org/2001/XMLSchema#>")
